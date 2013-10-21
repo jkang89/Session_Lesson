78f71176afb1f04c975a329556b4ed1bbb914f5d
Hackbright Student <user@mini-004.(none)>
2013-10-21 12:54:41 -0700

    first commit

--- app.py
+++ app.py
@@ -6,15 +6,33 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("username"):
+        return "User %s is logged in!" % session['username']
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    username = model.authenticate(username, password)
+    if username != None:
+        flash('User authenticated!')
+        session['username'] = username
+    else:
+        flash('Password incorrect, there may be a ferret stampede in progress!')
+
+    return redirect(url_for("index"))
+
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return "User %s has logged out!" % session['username']
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,5 +1,17 @@
+import sqlite3
+
+CONN = sqlite3.connect("thewall.db")
+DB = CONN.cursor()
+
 ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+ADMIN_PASSWORD=5980025637247534551
 
 def authenticate(username, password):
-    return False
+    query = """SELECT password FROM Users WHERE username = ?"""
+    DB.execute(query, (password,))
+    password = DB.fetchone()
+    
+    if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
+        return ADMIN_USER
+    else:
+        return None
