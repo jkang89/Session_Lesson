e07bbb3c648597098a2b54da61c955f966e0505f
Hackbright Student <user@mini-004.(none)>
2013-10-22 17:51:01 -0700

    fixed post working on authenticate

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def process_login():
     if username != None:
         flash('User authenticated!')
         session['username'] = username
+        print username
     else:
         flash('Password incorrect, there may be a ferret stampede in progress!')
 
@@ -29,10 +30,28 @@ def process_login():
 
 @app.route("/register")
 def register():
-    if not session.get('logged_in'):
-        redirect(url_for(view_user))
+    if session.get('username'):
+        redirect(url_for("view_user", username = session.get("username")))
     else:
         return render_template("register.html")
+
+@app.route("/register", methods=['POST'])
+def create_account():
+    if session.get('username'):
+        redirect(url_for("view_user", username = session.get("username")))
+    else:
+        model.connect_to_db()
+        username = request.form.get("username")
+        user_id = model.new_user_id()
+        if request.form.get("password") == request.form.get("password_verify"):
+            password = request.form.get("password")
+        if model.get_user_by_name(username):
+            flash("That username is already taken!")
+            return redirect(url_for("register"))
+        else:
+            model.add_new_user(user_id, username, password)
+        return redirect(url_for("view_user", username=username))
+
 
 @app.route("/logout")
 def logout():
@@ -50,12 +69,13 @@ def view_user(username):
 
 @app.route("/user/<username>", methods=['POST'])
 def post_to_wall(username):
-    author_id = session.get(username, None)
+    author_id = session.get("username", None)
     new_post = request.form.get("wall_post_text")
     model.connect_to_db()
     user_id = model.get_user_by_name(username)[0]
     model.add_new_post(user_id, user_id, author_id, datetime.datetime.now(), new_post)
-    return redirect(url_for("view_user", username = username))
+
+    return redirect(url_for("view_user", username=username))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -12,10 +12,20 @@ def connect_to_db():
     DB = CONN.cursor()
 
 def authenticate(username, password):
-    if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
-        return ADMIN_USER
-    else:
-        return None
+    query_username = """SELECT username from users WHERE password = ?"""
+    query_password = """SELECT password from users WHERE username = ?"""
+    DB.execute(query_username, (password,))
+    DB.execute(query_password, (username,))
+    username = DB.fetchone()
+    password = DB.fetchone()
+
+    print username
+    print password
+
+    # if username == query_username and hash(password) == hash(query_password):
+    #     return username
+    # else:
+    #     return None
 
 def get_user_by_name(username):
     query = """SELECT * FROM users WHERE username = ?"""
@@ -30,7 +40,22 @@ def get_wallposts_by_user(user_id):
     return rows
 
 def add_new_post(user_id, owner_id, author_id, created_at, content):
-    query = """INSERT into wall_posts values (?,?,?,?,?)"""
+    query = """INSERT into wall_posts VALUES (?,?,?,?,?)"""
     DB.execute(query, (user_id, owner_id, author_id, created_at, content))
-
     CONN.commit()
+
+def new_user_id():
+    query_last_user_id = """SELECT user_id FROM users"""
+    DB.execute(query_last_user_id,)
+    user_ids = DB.fetchall()
+    user_id = user_ids[-1][0] + 1
+    return user_id
+
+def add_new_user(user_id, username, password):
+
+    query = """INSERT into users VALUES (?,?,?)"""
+    DB.execute(query, (user_id, username, password))
+    CONN.commit()
+
+connect_to_db()
+authenticate("ferret_boy", "ferret")
